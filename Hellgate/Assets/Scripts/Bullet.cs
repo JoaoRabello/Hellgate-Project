f96b80940db7f399286fdb95065eaa4465be9808
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2019-08-20 22:31:49 -0300

    Enemy 2 + Project Reimport
    -Implemented the second enemy, a fly unit
    -Project is now on the Unity 19.2 (not beta this time)

---
+++ Hellgate/Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+    private Vector2 _direction;
+    public Vector2 Direction {set => _direction = value;}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_direction * 3f * Time.deltaTime);
+    }
+}
---
+++ Hellgate/Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    private bool _playerOnSight = false;
+    private GameObject _player;
+
+    private float _shootCounter = 1f;
+    private float _timeToShoot = 2f;
+
+    [SerializeField] private GameObject _bullet;
+
+    private void Update()
+    {
+        if (_playerOnSight)
+        {
+            CountAimShoot();
+        }
+    }
+
+    private void CountAimShoot()
+    {
+        if(_shootCounter >= _timeToShoot)
+        {
+            Vector2 dir = (_player.transform.position - transform.position).normalized;
+            Shoot(dir);
+            _shootCounter = 0;
+        }
+        else
+        {
+            _shootCounter += Time.deltaTime;
+        }
+    }
+
+    private void Shoot(Vector2 direction)
+    {
+        Bullet bullet = Instantiate(_bullet, transform).GetComponent<Bullet>();
+        bullet.Direction = direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _playerOnSight = true;
+            _player = col.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _playerOnSight = false;
+            _player = null;
+        }    
+    }
+}
