3a1ac4e33abaf1841802694efccd7926084b53f1
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2019-08-28 00:51:21 -0300

    Pause Menu
    -Added a Pause Menu to the main game
    -Modifications to the main menu canvas

---
+++ Hellgate/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseControl : MonoBehaviour
+{
+
+    private bool _paused = false;
+    [SerializeField] private GameObject _panel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!_paused)
+            {
+                Pause();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        _panel.SetActive(true);
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        _panel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
